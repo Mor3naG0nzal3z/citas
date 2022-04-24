dd05fffc33c0964b3549f3d3b414cd667af0eb1c
Alberto Santiago <al15760547@ite.edu.mx>
2022-04-23 19:10:36 -0700

    decompress zip

---
+++ Gestion de Citas/Gestion de Citas/Citas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Gestion_de_Citas
+{
+    public class Citas
+    {
+
+        public string Id { get; set; }
+
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-ddThh:mm:ss}")]
+        [Display(Name = "Registra tu Cita")]
+        public DateTime citas { get; set; }
+    }
+}  
